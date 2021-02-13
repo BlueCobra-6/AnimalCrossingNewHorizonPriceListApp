0632c8f489c74d290c7ee51557ae31e717eceb96
Tyler Larson <lars4629@umn.edu>
2021-02-13 11:10:10 -0600

    Rate app on times app opened

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:rate_my_app/rate_my_app.dart';
+
 import 'routes/pageRoutes.dart';
 import 'fragments/contactPage.dart';
 import 'fragments/animalPriceListPage.dart';
@@ -11,8 +13,26 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   static const String _title = 'Animal Crossing Price List';
 
+  final RateMyApp _rateMyApp = RateMyApp(
+      preferencesPrefix: 'rateMyApp_',
+      minDays: 7,
+      minLaunches: 50,
+      remindDays: 30,
+      remindLaunches: 100
+  );
+
+
+
   @override
   Widget build(BuildContext context) {
+    _rateMyApp.init().then((_){
+      if(_rateMyApp.shouldOpenDialog) { //conditions check if user already rated the app
+        _rateMyApp.showRateDialog(
+          context,
+          ignoreNativeDialog: false,
+        );
+      }
+    });
     return new MaterialApp(
       title: _title,
       theme: new ThemeData(
